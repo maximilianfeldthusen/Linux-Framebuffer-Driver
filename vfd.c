688fb4e8b33fb79f9ee908569a0349cdd4fdcc82
Maximilian Feldthusen <96908021+maximilianfeldthusen@users.noreply.github.com>
2025-10-28 08:01:29 +0100

    Update vfd.c

--- vfd.c
+++ vfd.c
@@ -1,1 +1,158 @@
 
+#include <linux/module.h>
+
+#include <linux/kernel.h>
+
+#include <linux/init.h>
+
+#include <linux/fb.h>
+
+#include <linux/vmalloc.h>
+
+#include <linux/uaccess.h>
+
+#define FB\_NAME "vfb"
+
+#define XRES 800
+
+#define YRES 600
+
+#define BPP 32
+
+static struct fb\_info \*vfb\_info;
+
+static u32 pseudo\_palette[16];
+
+static int vfb\_setcolreg(unsigned regno, unsigned red, unsigned green,
+
+`                         `unsigned blue, unsigned transp, struct fb\_info \*info) {
+
+`    `if (regno < 16) {
+
+`        `u32 val = ((red & 0xff00) << 8) | ((green & 0xff00) << 0) |
+
+`                  `((blue & 0xff00) >> 8);
+
+`        `pseudo\_palette[regno] = val;
+
+`        `return 0;
+
+`    `}
+
+`    `return -EINVAL;
+
+}
+
+static struct fb\_ops vfb\_ops = {
+
+.owner = THIS\_MODULE,
+
+.fb\_read = fb\_sys\_read,
+
+.fb\_write = fb\_sys\_write,
+
+.fb\_fillrect = sys\_fillrect,
+
+.fb\_copyarea = sys\_copyarea,
+
+.fb\_imageblit = sys\_imageblit,
+
+.fb\_setcolreg = vfb\_setcolreg,
+
+};
+
+static int \_\_init vfb\_init(void) {
+
+`    `int size = XRES \* YRES \* BPP / 8;
+
+`    `vfb\_info = framebuffer\_alloc(0, NULL);
+
+`    `if (!vfb\_info)
+
+`        `return -ENOMEM;
+
+`    `vfb\_info->screen\_base = vzalloc(size);
+
+`    `if (!vfb\_info->screen\_base) {
+
+`        `framebuffer\_release(vfb\_info);
+
+`        `return -ENOMEM;
+
+`    `}
+
+`    `vfb\_info->fbops = &vfb\_ops;
+
+`    `vfb\_info->fix = (struct fb\_fix\_screeninfo){
+
+.id = FB\_NAME,
+
+.type = FB\_TYPE\_PACKED\_PIXELS,
+
+.visual = FB\_VISUAL\_TRUECOLOR,
+
+.line\_length = XRES \* BPP / 8,
+
+.smem\_len = size,
+
+`    `};
+
+`    `vfb\_info->var = (struct fb\_var\_screeninfo){
+
+.xres = XRES,
+
+.yres = YRES,
+
+.bits\_per\_pixel = BPP,
+
+.red = {16, 8, 0},
+
+.green = {8, 8, 0},
+
+.blue = {0, 8, 0},
+
+.activate = FB\_ACTIVATE\_NOW,
+
+`    `};
+
+`    `vfb\_info->pseudo\_palette = pseudo\_palette;
+
+`    `if (register\_framebuffer(vfb\_info) < 0) {
+
+`        `vfree(vfb\_info->screen\_base);
+
+`        `framebuffer\_release(vfb\_info);
+
+`        `return -EINVAL;
+
+`    `}
+
+`    `printk(KERN\_INFO "Virtual framebuffer device registered\n");
+
+`    `return 0;
+
+}
+
+static void \_\_exit vfb\_exit(void) {
+
+`    `unregister\_framebuffer(vfb\_info);
+
+`    `vfree(vfb\_info->screen\_base);
+
+`    `framebuffer\_release(vfb\_info);
+
+`    `printk(KERN\_INFO "Virtual framebuffer device unregistered\n");
+
+}
+
+module\_init(vfb\_init);
+
+module\_exit(vfb\_exit);
+
+MODULE\_LICENSE("MIT");
+
+MODULE\_AUTHOR("Your Name");
+
+MODULE\_DESCRIPTION("Advanced Virtual Framebuffer Driver");
+
+
